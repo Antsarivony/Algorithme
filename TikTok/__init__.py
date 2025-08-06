4c0bac48db7bda7d5a097e3f770af72d9688d129
Antsarivony <normebee@gmail.com>
2025-08-06 21:12:56 +0300

    Add files via upload

---
+++ TikTok/__init__.py
@@ -0,0 +1,8 @@
+from TikTok.Argus import Argus
+from TikTok.Gorgon import Gorgon
+from TikTok.did import device_register
+from TikTok.Ladon import Ladon
+
+VERSION = "1.0"
+
+__all__ = ("Argus", "Gorgon", "device_register", "Ladon")
